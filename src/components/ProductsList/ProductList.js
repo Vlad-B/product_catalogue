e83d3b88dae55a2001ee321b24b8cd71934fe13b
Vlad-B <bogdanionutvlad@outlook.com>
2021-07-18 12:17:04 +0100

    Pass create new product function as prop to NewproductForm

--- src/components/ProductsList/ProductList.js
+++ src/components/ProductsList/ProductList.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
+import NewProductForm from '../NewProductForm/NewProductForm';
+import Product from '../Product/Product';
 
 export default class ProductList extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			productList: [],
+		};
+		this.create = this.create.bind(this);
+	}
+
+	create(newProduct) {
+		this.setState({
+			productList: [...this.state.productList, newProduct],
+		});
+	}
+
 	render() {
 		return (
 			<div>
 				<h1>Product Lookup App</h1>
+				<NewProductForm createProduct={this.create} />
+				<ul>Product list goes here</ul>
 			</div>
 		);
 	}
