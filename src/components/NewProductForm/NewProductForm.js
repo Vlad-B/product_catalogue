d8e4d80cea269ec8d2f0d6f51c0255c850b7be78
Vlad-B <bogdanionutvlad@outlook.com>
2021-07-18 12:29:05 +0100

    Create form, handleChange and handleSubmit for NewProductForm

--- src/components/NewProductForm/NewProductForm.js
+++ src/components/NewProductForm/NewProductForm.js
@@ -1,7 +1,68 @@
 import React, { Component } from 'react';
 
 export default class NewProductForm extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			productCode: '',
+			qty: '',
+			price: '',
+		};
+
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
+	handleChange(evt) {
+		this.setState({
+			[evt.target.name]: evt.target.value,
+		});
+	}
+
+	handleSubmit(evt) {
+		evt.preventDefault();
+		this.props.createProduct({ ...this.state });
+		this.setState({ productCode: '', qty: '', price: '' });
+	}
+
 	render() {
-		return <div></div>;
+		return (
+			<form className="NewProductForm" onSubmit={this.handleSubmit}>
+				<label htmlFor="productCode">Product Code</label>
+				<input
+					required
+					type="text"
+					placeholder="Add new product code"
+					value={this.state.productCode}
+					id="productCode"
+					name="productCode"
+					onChange={this.handleChange}
+				/>
+
+				<label htmlFor="qty">Quantity</label>
+				<input
+					required
+					type="text"
+					placeholder="Required quantity"
+					value={this.state.qty}
+					id="qty"
+					name="qty"
+					onChange={this.handleChange}
+				/>
+
+				<label htmlFor="price">Price</label>
+				<input
+					required
+					type="number"
+					placeholder="Price"
+					value={this.state.price}
+					id="price"
+					name="price"
+					onChange={this.handleChange}
+				/>
+
+				<button>Add Todo</button>
+			</form>
+		);
 	}
 }
